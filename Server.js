cbb5db283cc028a83f32e7c71d458d2b770c45b7
Mangal Mandor <mangalmandor8910@gmail.com>
2026-01-29 08:05:31 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config()
 const cors = require('cors');
+const cookie = require("cookie-parser")
 const DatabaseConnection = require("./configuration/Database.js")
 const donateKhoonRoutes = require("./routes/DonateRoute.js");
 const authRoute = require("./routes/AuthRoute.js")
@@ -8,7 +9,6 @@ const app = express()
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 const port = process.env.PORT || 3000;
-const cookie = require("cookie-parser")
 app.listen(port, () => {
     console.log(`Server start at ${port}`);
 })
@@ -32,3 +32,4 @@ app.get("/", (req, res) => {
     })
 
 })
+
